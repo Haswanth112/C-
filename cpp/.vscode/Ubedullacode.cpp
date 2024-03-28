11d30a0e0c10ba5205ce822170c66eb1215bd90c
Harshi <haswanthreddyambu@gmail.com>
2024-03-28 12:18:28 +0530

    modified

--- cpp/.vscode/Ubedullacode.cpp
+++ cpp/.vscode/Ubedullacode.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main()
 {
     vector<int>myvector;
-    myvector.push_back(0);
-    for(int i=1;i<=10;i++){
-        myvector.push_back(i);
+    int i;
+    myvector.push_back(3);
+    for(i=1;i<=10;i++){
+        myvector.push_back(i+5);
     }
     for(int vect:myvector)
     cout<<vect<<endl;
